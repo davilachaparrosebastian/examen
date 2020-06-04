05e137c91823d803d1df38e5089030a42055f89c
davilachaparrosebastian <64936039+davilachaparrosebastian@users.noreply.github.com>
2020-06-03 23:00:38 -0600

    Create controller.js

---
+++ controller.js
@@ -0,0 +1,24 @@
+ 
+let mongoose = require('mongoose');
+let Pintores = require('../models/Pintores');
+
+let PintoresController = {};
+
+/*LISTAR*/
+Pintores.list = (req, res)=>{
+    Pintores.find({})
+        .limit(20)
+        .skip(num:0)
+        .exec(callback:(err, Pintores)=>{
+            if (err){
+                console.log(err, pintor);
+            }
+            res.render('../views/pintores/index',{
+                Pintores: pintor,
+                title: "Listado de pintores",
+                year: new Date().getFullYear()
+            });
+        })
+};
+
+module.exports = PintoresController;
